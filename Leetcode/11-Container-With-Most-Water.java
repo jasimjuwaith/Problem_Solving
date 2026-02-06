2fae7b8e6de33a35498cbfa038907feb12816d8f
jasimjuwaith <jasimjuwaith783@gmail.com>
2026-02-06 21:05:40 +0530

    Resolved 1 warning

--- Leetcode/11-Container-With-Most-Water.java
+++ Leetcode/11-Container-With-Most-Water.java
@@ -3,7 +3,7 @@ class Solution {
         int left = 0;
         int right = height.length - 1;
         int maxArea = 0;
-        int currentArea = 0;
+        int currentArea;
         while(left < right){
             currentArea = Math.min(height[left], height[right]) * (right - left);
             maxArea = Math.max(currentArea, maxArea);
